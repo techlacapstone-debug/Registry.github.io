d4d7e572198daa3f7cc54ed5b9f34d67c5a0161f
perezjazaelr-web <perezjazaelr@gmail.com>
2025-09-26 21:26:19 +0800

    Initiatives

---
+++ capstone-techla/src/components/initiiative.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Initiiative() {
+  return (
+    <>
+        <div className='h-[25vh] bg-[#E1E6F3] p-[40px]'>
+            <div>
+                <h1 className='text-[18px] font-bold text-center color-[#007BFF]'>
+                    Household Registry Consists of Several Key Components and Initiatives
+                </h1>
+            </div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Initiiative
--- capstone-techla/src/pages/Landingpage.jsx
+++ capstone-techla/src/pages/Landingpage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Banner from '../components/banner'
+import Initiiative from '../components/initiiative'
 
 function Landingpage() {
   return (
-    <div className='h-screen bg-[#F2F5FF]'>
+    <div className='h-screen bg-[#F2F5FF] grid gap-2'>
         <Banner />
+        <div>
+          <Initiiative />
+        </div>
     </div>
   )
 }
