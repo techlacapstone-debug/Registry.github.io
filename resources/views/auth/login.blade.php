7836f3be4c761321ba782d25531ddd1fb2f1c0d8
GiAN0001 <132319947+GiAN0001@users.noreply.github.com>
2025-11-11 22:11:18 +0800

    login changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,39 +1,39 @@
 <x-guest-layout>
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <div class="login-form-card">
-        
-        <div class="login-logo">
-            <img src="{{ asset('images/logo-namayan.png') }}" alt="1N Namayan Logo">
+    <div class="login-form-card h-[55vh] w-[65%] px-[40px] flex justify-center items-center flex-col bg-[#FAFAFA] shadow-md rounded-[16px] gap-[32px]">
+
+        <div class="login-logo flex justify-center">
+            <img src="{{ asset('images/logo-namayan.png') }}" alt="1N Namayan Logo" class="w-[120px] h-[120px]">
         </div>
-        
-        <form method="POST" action="{{ route('login') }}">
+
+        <form method="POST" action="{{ route('login') }}" class="w-full">
             @csrf
 
             <div>
                 <x-input-label for="username" :value="__('Username')" />
                 <x-text-input id="username" class="block mt-1 w-full"
-                              type="text" 
-                              name="username" 
-                              :value="old('username')" 
-                              required 
-                              autofocus 
+                              type="text"
+                              name="username"
+                              :value="old('username')"
+                              required
+                              autofocus
                               autocomplete="username" />
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
             </div>
 
-            <div class="mt-4">
+            <div class="mt-[16px]">
                 <x-input-label for="password" :value="__('Password')" />
                 <x-text-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required 
+                                required
                                 autocomplete="current-password" />
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-primary-button class="ms-3">
+            <div class="mt-[32px]">
+                <x-primary-button class="h-[36px] w-[100%] bg-blue-700 flex justify-center">
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
